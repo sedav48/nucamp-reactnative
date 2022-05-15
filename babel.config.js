643697a2ca2c0aeb421ec86a24ba15ec6c13b872
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-05-15 17:42:41 -0400

    Week 1 Workshop-Reconfig

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
+export default function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-  };
+  
+    plugins: [
+    ...
+    'react-native-reanimated/plugin',
+  ]
+}
 };
