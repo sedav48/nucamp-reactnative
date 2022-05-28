966d59c5b7c1feb7707e96420447bf121d979c4e
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-05-28 17:25:09 -0400

    Week 3 Workshop FINAL FINAL

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -32,7 +32,7 @@ function RenderCampsite(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
-            view.currentrubberBand(1000)
+            view.current.rubberBand(1000)
             .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
         },
         onPanResponderEnd: (e, gestureState) => {
