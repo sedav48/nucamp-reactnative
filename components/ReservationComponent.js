4b7d66ad84cd72378f33ebc7509c03702e79e885
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-05-28 22:36:09 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,8 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
+import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
 
 class Reservation extends Component {
 
@@ -41,7 +43,7 @@ class Reservation extends Component {
             {
                 text: 'OK',
                 onPress: () => {
-                    this.presentLocalNotification(this.state.date);
+                    this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
                     this.resetForm();
                 }
             }
@@ -53,9 +55,35 @@ class Reservation extends Component {
         this.setState({
             campers: 1,
             hikeIn: false,
-            date: '',
+            date: new Date(),
+            showCalender: false
                
             });
+        }
+
+        async presentLocalNotification(date) {
+            function sendNotification() {
+                Notifications.setNotificationHandler({
+                    handleNotification: async () => ({
+                        shouldShowAlert: true
+                    })
+                });
+                Notifications.getAllScheduledNotificationsAsync({
+                    content: {
+                        title: 'Your Campsite Reservation Search',
+                        body: 'Search for ${date} requested'
+                    },
+                    trigger: null
+                });
+            }
+            
+            let permissions = await Notifications.getPermissionsAsync();
+            if (!permissions.granted) {
+                permissions = await Notifications.requestPermissionsAsync();
+            }
+            if (permissions.granted) {
+                sendNotification();
+            }
         }
     
 
